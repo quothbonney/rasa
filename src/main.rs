6ab702379ed01a2cb7feb5ddf3311cc75b3a6597
Jackie Carson <100622290+quothbonney@users.noreply.github.com>
2023-07-06 12:51:20 -0400

    Something is working.

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,7 @@ macro_rules! add_measurement {
     };
 }
 
+
 fn main() {
 
     let mut app = MonitorApp::new(20, 2);
@@ -35,45 +36,89 @@ fn main() {
     let monitor_ref = app.measurements.clone();
     let t_interval: u64 = 5;
 
-    let index = 0i32;
+    let mut index = 1i32;
     let port = "COM3";
     let baud_rate = 9600;
     let ports = serialport::available_ports().expect("No ports found!");
     println!("{:?}", ports);
+
+    let shared_var: Arc<RwLock<((f64, f64), (f64, f64))>> = Arc::new(RwLock::new(((0.0, 0.0), (0.0, 0.0))));
+
     let port = serialport::new(port, baud_rate)
         .timeout(Duration::from_millis(10))
         .open()
         .expect("Failed to open port");
-    let reader = std::io::BufReader::new(port);
+
+    let (tx, rx) = mpsc::channel();
+    let writer = thread::spawn(move || {
+        let reader = std::io::BufReader::new(port);
+        for line in reader.lines() {
+            match line {
+                Ok(line) => {
+                    // Here you can parse the line as per your serialization format.
+                    // Assuming it's a string of integers separated by spaces:
+                    let numbers: Vec<f64> = line
+                        .split_whitespace()
+                        .filter_map(|num| num.parse::<f64>().ok())
+                        .collect::<Vec<f64>>();
+                    //println!("{:?}", numbers);
+                    if numbers.len() >= 2 {
+                        let num = (
+                            (index as f64 / 100.0, numbers[0] as f64),
+                            (index as f64 / 100.0, numbers[1] as f64),
+                        );
+                         tx.send(num).unwrap();  // send processed value
+                    }
+                    index += 1;
+                    //thread::sleep(Duration::from_millis(1));
+                }
+                Err(err) => {
+                    eprintln!("Error: {}", err);
+                    continue;
+                }
+            }
+        }
+    });
+
+
+    let reader = thread::spawn(move || {
+        loop {
+            let mut last_received = None;
+
+            // Drain the channel and keep only the last received value
+            while let Ok(val) = rx.try_recv() {
+                last_received = Some(val);
+            }
+
+            if let Some(val) = last_received {
+                // Handle the received value
+                add_measurement!(monitor_ref, val.0, 0);
+                add_measurement!(monitor_ref, val.1, 1);
+            }
+
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+        /*
+        match vals {
+            Some(value) => {
+
+            }
+            _ => {
+                warn!("Could not read from input stream at index {}", index);
+            }
+        };
+        */
+/*
+
+
 
     thread::spawn(move || {
         //let stdin = std::io::stdin();
         let mut index: i32 = 0;
         let mut start = Instant::now();
-
-
-        for line in reader.lines() {
-            let vals: Option<((f64, f64), (f64, f64))>;
-            match line {
-                Ok(line) => {
-                    // Here you can parse the line as per your serialization format.
-                    // Assuming it's a string of integers separated by spaces:
-                    let numbers: Vec<i32> = line
-                        .split_whitespace()
-                        .filter_map(|num| num.parse::<i32>().ok())
-                        .collect();
-                    println!("{:?}", numbers);
-                    vals = Some((
-                        (index as f64 / 10.0, numbers[0] as f64),
-                        (index as f64 / 10.0, numbers[1] as f64),
-                    ));
-                    thread::sleep(Duration::from_millis(1));
-                }
-                Err(err) => {
-                    eprintln!("Error: {}", err);
-                    continue;
-                },
-            }
+        let elapsed = start.elapsed();
 
             match vals {
                 Some(value) => {
@@ -87,7 +132,7 @@ fn main() {
             index += 1;
         }
     });
-
+*/
     info!("Main thread started");
     eframe::run_native("Monitor app", native_options, Box::new(|_| Box::new(app)));
 }
